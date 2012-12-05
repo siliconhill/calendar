f2fb11e58fcc76cdfc77b8d2bd09ce3b457eb111
Josef Kříž <pepakriz@gmail.com>
2012-12-05 21:00:42 +0100

    Základní implementace login formuláře.

---
+++ app/forms/LoginFormFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+use Nette\Object;
+use Nette\Forms\Controls\Button;
+use Nette\Application\UI\Form;
+
+class LoginFormFactory extends Object
+{
+
+	/**
+	 * @return Nette\Application\UI\Form
+	 */
+	public function create()
+	{
+		$form = new Form();
+		$form->addText('name', 'Name');
+		$form->addPassword('password', 'Password');
+		$form->addSubmit('_submit', 'Login')->onClick[] = $this->handleSuccess;
+
+		return $form;
+	}
+
+
+	public function handleSuccess(Button $button)
+	{
+		/** @var $form Form */
+		$form = $button->form;
+
+		try {
+			$form->presenter->user->login($form['name']->value, $form['password']->value);
+		} catch (\Nette\Security\AuthenticationException $e) {
+			$form->addError($e->getMessage());
+		}
+	}
+}
--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -6,4 +6,29 @@
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
 
+	/** @var LoginFormFactory */
+	protected $loginFormFactory;
+
+
+	/**
+	 * @param LoginFormFactory $loginFormFactory
+	 */
+	public function injectLoginFormFactory(LoginFormFactory $loginFormFactory)
+	{
+		$this->loginFormFactory = $loginFormFactory;
+	}
+
+
+	public function createComponentLogin()
+	{
+		$form = $this->loginFormFactory->create();
+		$form->onSuccess[] = $this->loginSuccess;
+		return $form;
+	}
+
+
+	public function loginSuccess()
+	{
+		$this->flashMessage('Uživatel byl vpořádku přihlášen', 'success');
+	}
 }
